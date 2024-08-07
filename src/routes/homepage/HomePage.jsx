4f1e031d14d81c7f7d7b6f237b1d9a8f3a5f5bca
Hansana Sandakelum <95872422+HansanaSandakelum@users.noreply.github.com>
2024-08-07 15:15:02 +0530

    Update layout to include Navbar and HomePage components

--- src/routes/homepage/HomePage.jsx
+++ src/routes/homepage/HomePage.jsx
@@ -1,1 +1,23 @@
-fcs
+import "./homePage.scss";
+function HomePage() {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique
+            maxime aut incidunt iste tempora alias dignissimos ducimus
+            praesentium odio ea, quaerat labore amet facilis autem dolorem,
+            assumenda delectus nam culpa!
+          </p>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
